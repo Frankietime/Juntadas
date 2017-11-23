e36d7b3b024f1cc7edd81ac3a4efe9ccde6edab5
frankietime <donadiofranco7@gmail.com>
2017-11-23 15:56:53 -0300

    added FAB, polished addNewJuntada Data Step UI design and added a FAB for those steps

--- Juntadas/www/js/controllers.js
+++ Juntadas/www/js/controllers.js
@@ -1,10 +1,11 @@
 angular.module('app.controllers', [])
 
-    .controller('homeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+    .controller('homeCtrl', ['$scope', '$stateParams', '$state', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
         // You can include any angular dependencies as parameters for this function
         // TIP: Access Route Parameters for your page via $stateParams.parameterName
-        function ($scope, $stateParams) {
+        function ($scope, $stateParams, $state) {
 
+            //$scope.event = function () { return $state.go('tabsController.addJuntadaDataStep'); };
 
         }])
 
